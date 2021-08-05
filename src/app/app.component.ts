e33023e434fa8b2646533f049c7f10f84a6a518a
Kubilay Kaplan <d_kubilayk@tofas.com.tr>
2021-08-05 19:32:36 +0300

    Angular giriş ve basit tablo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'FinalProjectUI';
+  title:string = 'FinalProjectUI';
+  user:string ='enginDemiroğ';
+  
 }
