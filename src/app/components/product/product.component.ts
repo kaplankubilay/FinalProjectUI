60b83c71fa2d18076988a4748cd0bd971c94e7ff
Kubilay Kaplan <d_kubilayk@tofas.com.tr>
2021-08-07 18:41:30 +0300

    Nesnenin observable edilmesi sağlandı ve httpClient kullanıldı.

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product';
+import { ProductResponseModel } from 'src/app/models/productResponseModel';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-product',
@@ -8,11 +10,25 @@ import { Product } from 'src/app/models/product';
 })
 export class ProductComponent implements OnInit {
   
-  products:Product[]=[]
+  products:Product[]=[];
+  apiUrl:string="https://localhost:44381/api/products/getall";
 
-  constructor() { }
+  // productResponseModel:ProductResponseModel={
+  //   data : this.products,
+  //   message:"",
+  //   success:true
+  // };
+
+  constructor(private httpClient:HttpClient) { }
 
   ngOnInit(): void {    
+    this.getProgucts();
+  }
+
+  getProgucts(){
+    this.httpClient.get<ProductResponseModel>(this.apiUrl).subscribe((response)=>{
+      this.products = response.data
+    });
   }
 
 }
